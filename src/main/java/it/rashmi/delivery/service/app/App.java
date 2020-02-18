0f0b825cbc17c0dc9d52c66d0a6bf176a4768cee
rashmiprabhath <rashmiprabhath@gmail.com>
2020-02-19 00:06:18 +0530

    DS:API: Initial Commit

---
+++ src/main/java/it/rashmi/delivery/service/app/App.java
@@ -0,0 +1,9 @@
+package it.rashmi.delivery.service.app;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/it/rashmi/delivery/service/controllers/ProductController.java
@@ -0,0 +1,8 @@
+package it.rashmi.delivery.service.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+
+}
