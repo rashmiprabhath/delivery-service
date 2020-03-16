8571ef8ec7ec100a4c930dd2288200fb81c2c9fe
rashmiprabhath <rashmiprabhath@gmail.com>
2020-03-17 00:33:51 +0530

    DS:API: configuration controller.

--- src/main/java/it/rashmi/delivery/controllers/ConfigurationController.java
+++ src/main/java/it/rashmi/delivery/controllers/ConfigurationController.java
@@ -14,6 +14,18 @@ public class ConfigurationController
     @Autowired
     private ConfigurationService configurationService;
 
+    @GetMapping( "/delivery_status" )
+    public List<ConfigParamModel> getAllDeliveryStatus()
+    {
+        return this.configurationService.getDeliveryStatuses();
+    }
+
+    @PostMapping( "/delivery_status" )
+    public void saveDeliveryStatus( @RequestParam( value = "name" ) String name )
+    {
+        this.configurationService.saveDeliveryStatus( name );
+    }
+
     @GetMapping( "/items_status" )
     public List<ConfigParamModel> getAllItemStatus()
     {
@@ -24,5 +36,53 @@ public class ConfigurationController
     public void saveItemStatus( @RequestParam( value = "name" ) String name )
     {
         this.configurationService.saveItemStatus( name );
+    }
+
+    @GetMapping( "/payment_status" )
+    public List<ConfigParamModel> getAllPaymentStatuses()
+    {
+        return this.configurationService.getPaymentStatuses();
+    }
+
+    @PostMapping( "/payment_status" )
+    public void savePaymentStatus( @RequestParam( value = "name" ) String name )
+    {
+        this.configurationService.savePaymentStatus( name );
+    }
+
+    @GetMapping( "/product_type" )
+    public List<ConfigParamModel> getAllProductTypes()
+    {
+        return this.configurationService.getProductTypes();
+    }
+
+    @PostMapping( "/product_type" )
+    public void saveProductType( @RequestParam( value = "name" ) String name )
+    {
+        this.configurationService.saveProductType( name );
+    }
+
+    @GetMapping( "/supplier_type" )
+    public List<ConfigParamModel> getAllSupplierType()
+    {
+        return this.configurationService.getSupplierTypes();
+    }
+
+    @PostMapping( "/supplier_type" )
+    public void saveSupplierType( @RequestParam( value = "name" ) String name )
+    {
+        this.configurationService.saveSupplierType( name );
+    }
+
+    @GetMapping( "/user_level" )
+    public List<ConfigParamModel> getAllUserLevel()
+    {
+        return this.configurationService.getUserLevels();
+    }
+
+    @PostMapping( "/user_level" )
+    public void saveUserLevel( @RequestParam( value = "name" ) String name )
+    {
+        this.configurationService.saveUserLevel( name );
     }
 }
--- src/main/java/it/rashmi/delivery/service/ConfigurationService.java
+++ src/main/java/it/rashmi/delivery/service/ConfigurationService.java
@@ -1,8 +1,8 @@
 package it.rashmi.delivery.service;
 
-import it.rashmi.delivery.entity.ItemStatus;
+import it.rashmi.delivery.entity.*;
 import it.rashmi.delivery.model.ConfigParamModel;
-import it.rashmi.delivery.repository.ItemStatusRepository;
+import it.rashmi.delivery.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +13,40 @@ import java.util.stream.Collectors;
 public class ConfigurationService
 {
     @Autowired
+    private DeliveryStatusRepository deliveryStatusRepository;
+    @Autowired
     private ItemStatusRepository itemStatusRepository;
+    @Autowired
+    private PaymentStatusRepository paymentStatusRepository;
+    @Autowired
+    private ProductTypeRepository productTypeRepository;
+    @Autowired
+    private SupplierTypeRepository supplierTypeRepository;
+    @Autowired
+    private UserLevelRepository userLevelRepository;
+
+    /** ------------ delivery status services ---------------- */
+
+    public List<ConfigParamModel> getDeliveryStatuses()
+    {
+        return deliveryStatusRepository.findAll().stream()
+                                   .map( config -> new ConfigParamModel( config.getId(), config.getName() ) )
+                                   .collect( Collectors.toList() );
+    }
+
+    public void saveDeliveryStatus( String name )
+    {
+        DeliveryStatus deliveryStatus = new DeliveryStatus();
+        deliveryStatus.setName( name );
+        this.deliveryStatusRepository.save( deliveryStatus );
+    }
+
+    /** ------------ item status services ---------------- */
 
     public List<ConfigParamModel> getItemStatuses()
     {
         return itemStatusRepository.findAll().stream()
-                                   .map( status -> new ConfigParamModel( status.getId(), status.getName() ) )
+                                   .map( config -> new ConfigParamModel( config.getId(), config.getName() ) )
                                    .collect( Collectors.toList() );
     }
 
@@ -28,4 +56,69 @@ public class ConfigurationService
         itemStatus.setName( name );
         this.itemStatusRepository.save( itemStatus );
     }
+
+    /** ------------ payment status services ---------------- */
+
+    public List<ConfigParamModel> getPaymentStatuses()
+    {
+        return paymentStatusRepository.findAll().stream()
+                                   .map( config -> new ConfigParamModel( config.getId(), config.getName() ) )
+                                   .collect( Collectors.toList() );
+    }
+
+    public void savePaymentStatus( String name )
+    {
+        PaymentStatus paymentStatus = new PaymentStatus();
+        paymentStatus.setName( name );
+        this.paymentStatusRepository.save( paymentStatus );
+    }
+
+    /** ------------ product type services ---------------- */
+
+    public List<ConfigParamModel> getProductTypes()
+    {
+        return productTypeRepository.findAll().stream()
+                                    .map( config -> new ConfigParamModel( config.getId(), config.getName() ) )
+                                    .collect( Collectors.toList() );
+    }
+
+    public void saveProductType( String name )
+    {
+        ProductType productType = new ProductType();
+        productType.setName( name );
+        this.productTypeRepository.save( productType );
+    }
+
+    /** ------------ supplier type services ---------------- */
+
+    public List<ConfigParamModel> getSupplierTypes()
+    {
+        return supplierTypeRepository.findAll().stream()
+                                    .map( config -> new ConfigParamModel( config.getId(), config.getName() ) )
+                                    .collect( Collectors.toList() );
+    }
+
+    public void saveSupplierType( String name )
+    {
+        SupplierType supplierType = new SupplierType();
+        supplierType.setName( name );
+        this.supplierTypeRepository.save( supplierType );
+    }
+
+    /** ------------ user level services ---------------- */
+
+    public List<ConfigParamModel> getUserLevels()
+    {
+        return userLevelRepository.findAll().stream()
+                                    .map( config -> new ConfigParamModel( config.getId(), config.getName() ) )
+                                    .collect( Collectors.toList() );
+    }
+
+    public void saveUserLevel( String name )
+    {
+        UserLevel userLevel = new UserLevel();
+        userLevel.setName( name );
+        this.userLevelRepository.save( userLevel );
+    }
+
 }
